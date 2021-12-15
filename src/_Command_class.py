3aa8dd8e1fb76a7fb472fc4d6955d3a74b9540d0
Hervé PRECIGOUT <hyper.poubelle@gmail.com>
2021-12-15 22:36:09 +0100

    Utilisation de la classe Command

--- src/_Command_class.py
+++ src/_Command_class.py
@@ -5,42 +5,58 @@ from module_globals import *
 # Definition de la classe Command
 
 class Command:
-    def __init__(self, cmdline=""):
-        self.__cmdline = cmdline
+    def __init__(self, order=0, command_line="", wait_after=False):
+        self.__order = order
+        self.__command_line = command_line
+        self.__wait_after = wait_after
         self.__last_return_code = 0
 
     # Accesseurs
-    def get_cmdline(self):
-        return self.__cmdline
+    def get_order(self):
+        return self.__order
+
+    def get_command_line(self):
+        return self.__command_line
 
     def get_last_return_code(self):
         return self.__last_return_code
 
     # Mutateurs
-    def set_cmdline(self, cmdline):
-        self.__cmdline = str(cmdline)
+    def set_order(self, order):
+        self.__order = order
 
-    # Methodes privees
+    def set_command_line(self, command_line):
+        self.__command_line = str(command_line)
 
-    # Methodes publiques
-    def execute(self, cmdline="", whatif=False):
-        if cmdline != "":
-            self.__cmdline = cmdline
-        print_fmt(cmdline, "COMMAND")
-        if whatif:
-            print_fmt("*** Whatif *** : la commande [" + self.__cmdline + "] n'a pas ete executee", format="YELLOW", indent=4)
+    def set_wait_after(self, wait_after):
+        self.__wait_after = wait_after
+
+    # Méthodes privées
+
+    # Méthodes publiques
+    def execute(self, command_line="", what_if=False):
+        if command_line != "":
+            self.__command_line = command_line
+        self.print_command_line()
+        if what_if:
+            print_fmt("*** what_if *** : la commande [" + self.__command_line + "] n'a pas été exécutée",
+                      format="YELLOW",
+                      indent=4
+                      )
         else:
-            print_fmt("Execution de la commande [" + self.__cmdline + "]", format="YELLOW", indent=4)
-            self.__last_return_code = os.system(self.__cmdline)
+            print_fmt("Execution de la commande [" + self.__command_line + "]",
+                      format="YELLOW",
+                      indent=4
+                      )
+            self.__last_return_code = os.system(self.__command_line)
         return self.__last_return_code
 
-    def print_cmdline(self):
-        print_fmt(self.__cmdline, "COMMAND", 4)
+    def print_command_line(self):
+        print_fmt(self.__command_line, "COMMAND", 4)
 
     def print(self):
-        print_fmt("Commande", "TITRE1")
-        print_fmt("Ligne de commande", "TITRE2")
-        self.print_cmdline()
+        self.print_command_line()
+
 
 #
 # =================================================================================================
@@ -49,6 +65,6 @@ class Command:
 if __name__ == "__main__":
     my_command = Command()
 
-    my_command.set_cmdline("ls -la")
+    my_command.set_command_line("ls -la")
     my_command.print()
     print(my_command.execute())
